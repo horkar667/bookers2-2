2de58bc6d376b01dc38395b97e0f499cf3ad023d
horkar667 <jmmpp413@yahoo.co.jp>
2021-09-07 06:55:36 +0000

    [Add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get '/home/about' => "homes#about"
-  root to: 'homes#top'
+  get 'favoretes/create'
+  get 'favoretes/destroy'
   devise_for :users
+  root to: 'homes#top'
+  get '/home/about' => "homes#about"
   resources :users, only: [:index,:show,:edit,:update]
-  resources :books, only: [:index,:show,:create,:destroy,:edit,:update]
+  resources :books, only: [:index,:show,:create,:destroy,:edit,:update] do
+    resource :favorites, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/favoretes_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class FavoretesControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get favoretes_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get favoretes_destroy_url
+    assert_response :success
+  end
+
+end
