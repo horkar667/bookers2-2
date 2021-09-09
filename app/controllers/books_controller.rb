198851bbd08f419a402f01ddd8e3a1c0c6a774d9
horkar667 <jmmpp413@yahoo.co.jp>
2021-09-09 07:33:20 +0000

    [Add]星・評価機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
     private
 
   def book_params
-    params.require(:book).permit(:title,:body,:user_id)
+    params.require(:book).permit(:title,:body,:user_id,:rate)
   end
 
   def ensure_current_user
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true, length: {maximum: 200}
+
   belongs_to :user
 
   has_many :favorites, dependent: :destroy
@@ -11,4 +12,5 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+
 end
